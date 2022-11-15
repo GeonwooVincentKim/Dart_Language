5b6d23f8ac4a4e10e70e25fda647994bb678c9af
GeonwooVincentKim <kdsnop@gmail.com>
2022-11-15 11:41:23 +0900

    [Feature] - Then Function.dart

---
+++ bin/02/then_function.dart
@@ -0,0 +1,18 @@
+import 'async_handling.dart';
+
+void main() async {
+  await getVersionName().then((value) => {
+    print(value)
+  });
+
+  print('end process');
+}
+
+Future<String> getVersionName() async {
+  var versionName = await lookUpVersion();
+  return versionName;
+}
+
+String lookUpVersion() {
+  return 'Android Q';
+}
